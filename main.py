1a6db54d53b53a8424382d10df146d286f6e0b13
Juan-1406 <juan.alvarado01@alumnos.ucn.cl>
2026-06-29 17:46:31 -0400

    Generador de Texto (N-Gram)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.preprocesamiento.preprocesador_texto import PreprocesadorTexto
 from src.preprocesamiento.analizador_vocabulario import AnalizadorVocabulario
 from src.representacion.tfidf import VectorTF_IDF
 from src.visualizacion.exploratorio import VisualizacionYExplorador
+from src.generadorTexto.generador_texto import ModeloNGramas
 
 def main():
     cargador = CargadorDatos()
@@ -20,6 +21,23 @@ def main():
     explorador = VisualizacionYExplorador(df)
     explorador.graficoLongitud_Versiculos()
     explorador.graficoVersiculos_por_Libro()
+
+    print("\n=== 3.6 GENERADOR DE TEXTO ===")
+    modelos = {
+        "Unigram": ModeloNGramas(1),
+        "Bigram": ModeloNGramas(2),
+        "Trigram": ModeloNGramas(3),
+        "4-gram": ModeloNGramas(4)
+    }
+
+    for nombre, modelo in modelos.items():
+        modelo.entrenar(df["tokens"])
+        
+    print("\n--- Versículos Falsos ---")
+    palabraInicio = "shall"
+    for nombre, modelo in modelos.items():
+        texto = modelo.generarTexto(palabraInicial = palabraInicio, longitudMax=15)
+        print(f"{nombre}: [{texto}]")
 
 if __name__ == "__main__":
     main()
